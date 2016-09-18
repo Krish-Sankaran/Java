e57df26d7905752ef923629e53b3d0b8a34d5a51
Krish-Sankaran <ksankaran2107@gmail.com>
2016-09-17 17:47:36 -0700

    added test file

---
+++ TestSuff.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agmo2_000
+ */
+public class TestSuff {
+    int n;
+    int a;
+    int b;
+    int c;
+    String answer;
+    
+    public TestSuff(int count){
+        n = count;
+        a = 0;
+        b = 1;
+    }
+    
+    public void doThings(){
+        if (n == 1){
+            System.out.print(a + " ");
+        }
+        else if (n == 2){
+            System.out.print(a + " " + b + " ");
+        }
+        else {
+            System.out.print(a + " " + b + " ");
+            for(int i = 2; i < n; i++){
+                c = a+b;
+                a = b;
+                b = c;
+                System.out.print(c + " ");
+            }
+        }
+    }
+    
+}
---
+++ TestSuffDriver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agmo2_000
+ */
+public class TestSuffDriver {
+    
+    public static void main(String[]args){
+        TestSuff test = new TestSuff(30);
+        test.doThings();
+        //System.out.println(x);
+    }
+    
+}
